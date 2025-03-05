ac14d52407d4c74e9e3d1819832bf0954ca76227
SamuelB0X <samueloviedo397@gmail.com>
2025-03-04 19:26:03 -0500

    main

--- src/Droid.java
+++ src/Droid.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
-
 public class Droid {
+    int batteryLevel;
+    String name;
+
+    public Droid (String droidName) {
+        name = droidName;
+        batteryLevel = 100;
+    }
+    public String toString() {
+        return "Hello, my name is " + name;
+    }
+
+    public void energyReport(){
+        System.out.println("Current battery level: "+ batteryLevel);
+    }
+
+    public void performTask(String task) {
+        System.out.println(name + " is performing: " + task);
+        batteryLevel -= 10;
+        energyReport();
+    }
+
 }
+
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
-
 public class Main {
+    public static void main(String[] args) {
+        Droid myDroid = new Droid("Codey");
+
+        System.out.println(myDroid);
+
+        myDroid.performTask ("cleaning out my closet...");
+    }
 }
